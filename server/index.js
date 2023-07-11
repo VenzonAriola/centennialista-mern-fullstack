187f6ecc959f094060030cb000626eba074b042f
MightyBeast <variola@my.centennialcollege.ca>
2023-07-11 15:40:22 -0400

    updated cors origin

--- server/index.js
+++ server/index.js
@@ -24,13 +24,18 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
+
+const corsOption = {
+  origin: "https://vercel.com/mightybeast89/centennialista-mern-fullstack",
+};
+
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(corsOption));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
