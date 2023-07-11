c0a5dc52c6271cbe79cc7177a084c7fdda37ab3b
MightyBeast <variola@my.centennialcollege.ca>
2023-07-11 17:06:31 -0400

    fix cors

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ dotenv.config();
 const app = express();
 
 const corsOption = {
-  origin: "https://centennialista-mern-fullstack.vercel.app/",
+  origin: "https://centennialista-mern-fullstack.vercel.app",
   allowedHeaders: true,
   credentials: true,
 };
